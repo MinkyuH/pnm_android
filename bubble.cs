b1d41638fdff3d746d83fa1e4e2d4e1ebf9c7794
Steven <kan02100@naver.com>
2017-09-06 12:40:42 +1200

    Error fixed for rb2d

---
+++ bubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private bool lifecondition = false;
+    public float upForce = 200f;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (lifecondition == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+    void Collision()
+    {
+        lifecondition = true;
+    }
+}
