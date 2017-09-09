02059098d6e901686359070d06f8895db7532eb6
Steven <kan02100@naver.com>
2017-09-09 21:37:24 +1200

    Obstacles and scrollingObject scripts added

---
+++ Obstacles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacles : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<UserScripttwo>() != null)
+        {
+            GameController.instance.Score();
+        }
+    }
+}
